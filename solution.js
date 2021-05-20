333352a17c2db57acaa974f1472f2c616221f6e7
Nuala O'Donovan <75636743+nuala-odonovan@users.noreply.github.com>
2021-05-20 17:24:05 -0400

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,51 @@
+const ones = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'
+}
+const tens = {
+    10: 'ten',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety'
+}
+const teens = {
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen'
+}
+
+//helper function to convert integers to string value
+function numToString(value){
+  if(value < 10) return ones[value]
+  if(value > 10 && value < 20 ) value = teens[value]
+  if(value % 10 === 0 ) value = tens[value]
+  else return tens[value - (value%10)] +  " " + ones[value%10]
+}
+
+function priceToString(value) {
+  let dollars = parseInt(value)
+  let cents = parseInt((value % 1).toFixed(2) * 100)
+  
+  if(dollars===0) return `${numToString(cents)} cents`
+  else if(cents===0) return `${numToString(dollars)} dollars`
+  else return `${numToString(dollars)} dollars and ${numToString(cents)} cents`
+}
